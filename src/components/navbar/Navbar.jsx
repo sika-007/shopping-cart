427acd00963c2cf21ec533c151fc5a43e19efc81
Nsikak Thomas <nsikakthomas102@gmail.com>
2023-02-15 22:33:14 +0100

    selected page indicator function and styling added

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import { ShoppingCart } from 'phosphor-react'
 import "./navbar.css"
 
 const Navbar = () => {
+  const [selectedPage, setSelectedPage] = useState("shop")
+
+  function cartPageSelect() {
+    setSelectedPage("cart")
+  }
+
+  function shopPageSelect() {
+    setSelectedPage("shop")
+  }
+
   return (
     <div className='navbar'>
       <div className="navbar__title">
@@ -11,10 +21,14 @@ const Navbar = () => {
       </div>
       <div className="navbar__links">
         <Link to="/">
+         <div onClick={shopPageSelect} className={selectedPage === "shop" ? "selectedBG" : "noBG"}>
             Shop
+          </div>
         </Link>
         <Link to="/cart">
+          <div onClick={cartPageSelect} className={selectedPage === "cart" ? "selectedBG" : "noBG"}>
             <ShoppingCart size={32}/>
+          </div>
         </Link>
       </div>
     </div>
