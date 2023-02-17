178602b71752c64753a52d9e55a00a73bbeb9a02
Nsikak Thomas <nsikakthomas102@gmail.com>
2023-02-17 19:32:03 +0100

    Bug fix: use of async function to fetch

--- src/productData.js
+++ src/productData.js
@@ -1,14 +1,22 @@
 import axios from "axios"
 
 
-let products = []
+async function getProducts() {
+    return new Promise((accept, reject) => {
+        const productsArr = []
+        axios.get("https://fakestoreapi.com/products?limit=20")
+        .then(res => {
+            productsArr.push(...res.data)
+            accept(productsArr)
+        }).catch(err => {
+            console.log(err)
+        })
+    })
+    
+}
 
-axios.get("https://fakestoreapi.com/products?limit=20")
-.then(res => {
-    products.push(...res.data)
-}).catch(err => {
-    console.log(err)
-})
+const products = await getProducts()
 
 export default products
+
 
